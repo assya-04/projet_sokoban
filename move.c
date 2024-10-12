d0b6ccf08e5a661bc884e83e359a71b50fe3fb92
assya-04 <assiatou.drame@coda-student.school>
2024-10-12 17:30:31 +0200

    fonction des mouvements

--- move.c
+++ move.c
@@ -1,5 +1,6 @@
 #include "sokoban.h"
 
+
 char ask_player_move() 
 {
     char move;
@@ -8,45 +9,35 @@ char ask_player_move()
     return move;
 }
 
-int deplacer(char tab[SIZE][SIZE], joueur *j, char move) 
+int deplacer(char tab[SIZE][SIZE], joueur *j, char move, box *b, empl *em) 
 {
     int new_x = j->x;
     int new_y = j->y;
 
-    if(move=='w')
-    {
-        new_x=new_x-1;
-    }
-    else if(move=='s')
-    {
-        new_x=new_x+1;
-    }
-    else if(move=='a')
-    {
-        new_y=new_y-1;
-    }
-    else if(move=='d')
-    {
-        new_y=new_y+1;
-    }
-    else
-    {
-        return 0;
+    if(move == 'w') {
+        new_x--;
+    } else if(move == 's') {
+        new_x++;
+    } else if(move == 'a') {
+        new_y--;
+    } else if(move == 'd') {
+        new_y++;
+    } else {
+        return 0; // Mouvement invalide
     }
 
-
+    // Déplacer le joueur si possible
     if (tab[new_x][new_y] == ' ' || tab[new_x][new_y] == EMPLACEMENT) {
         // Déplacer le joueur
         tab[j->x][j->y] = ' ';
         j->x = new_x;
         j->y = new_y;
         tab[j->x][j->y] = PLAYER;
-        return 1;
-    } else if (tab[new_x][new_y] == BOX) 
-    {
+    } else if (tab[new_x][new_y] == BOX) {
         // Vérifier si on peut pousser la boîte
         int box_new_x = new_x + (new_x - j->x);
         int box_new_y = new_y + (new_y - j->y);
+        
         if (tab[box_new_x][box_new_y] == ' ' || tab[box_new_x][box_new_y] == EMPLACEMENT) {
             // Pousser la boîte
             tab[box_new_x][box_new_y] = BOX;
@@ -54,8 +45,30 @@ int deplacer(char tab[SIZE][SIZE], joueur *j, char move)
             tab[j->x][j->y] = ' ';
             j->x = new_x;
             j->y = new_y;
-            return 1;
+            // Mettre à jour la position de la boîte
+            b->x = box_new_x;
+            b->y = box_new_y;
+        } else {
+            return 0; // Mouvement impossible
         }
+    } else {
+        return 0; // Mouvement impossible
     }
-    return 0; // mouvement impossible
+
+    // Vérification de la victoire
+    if (b->x == em->x && b->y == em->y) {
+        printf("Félicitations vous avez gagné !\n");
+        exit(0);
+    }
+
+    // Vérification de la défaite
+    if ((b->x == 1 && b->y == 1) ||
+        (b->x == 1 && b->y == (SIZE - 1)) ||
+        (b->x == (SIZE - 1) && b->y == 1) ||
+        (b->x == (SIZE - 1) && b->y == (SIZE - 1))) {
+        printf("Boîte coincée, vous avez perdu !\n");
+        exit(0);
+    }
+
+    return 1; // Mouvement réussi
 }
